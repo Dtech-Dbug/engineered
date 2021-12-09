d66ff214761c834837e41677a30b15d60a8f9996
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-12-09 21:37:16 +0530

    template for Board

---
+++ client/Ui_Components/Board.js/Board.js
@@ -0,0 +1,13 @@
+import "../../UI_Components_Styles/board.css";
+
+function Board() {
+  return (
+    <div>
+      <h1 contenteditable spellcheck="false">
+        hire(d) in a Flash
+      </h1>
+    </div>
+  );
+}
+
+export default Board;

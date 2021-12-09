c253ea17acf567be0f0267e810654b3c726fb56a
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-12-09 22:45:00 +0530

    created component: Nav.jsx

---
+++ client/src/Components/Nav.jsx
@@ -0,0 +1,13 @@
+export const Nav = () => {
+  return (
+    <nav>
+      <h3>Logo</h3>
+
+      <ul>
+        <li>About</li>
+        <li>Categories</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};

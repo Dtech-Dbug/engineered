79c729c9b13e1c36126bddf9304ff839a749b684
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-12-11 22:36:02 +0530

    Header Component updated: deleted redundant divs

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,8 +1,12 @@
+// import image from "../Assets/outer-space.jpg";
+
 function Header() {
   return (
-    <div>
-      <h1>Engineered By Engineers</h1>
-    </div>
+    <>
+      <div>
+        <h1>Engineered By Engineers</h1>
+      </div>
+    </>
   );
 }
 
